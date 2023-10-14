7d2b2fcf5910d9c9d9213d5eaa031ecb6437b752
aryanofficialjain <aryanofficialjain@gmail.com>
2023-10-14 07:42:45 +0530

    adding all the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Coin from "./Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, []);
+
+  const handlechange = e => {
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search Crypto</h1>
+        <form>
+          <input type="text" placeholder="Search Here" className="coin-input" onChange={handlechange} />
+        </form>
+      </div>
+      {filteredCoins.map(coin => (
+        <Coin
+          key={coin.id}
+          name={coin.name}
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap} // Updated property name
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume} // Updated property name
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Coin.css'
+const Coin = ({name, price, symbol, image, volume, priceChange, marketcap}) => {
+  return (
+    <div className='coin-container'>
+        <div className="coin-row">
+            <div className="coin">
+                <img src={image} alt="cryto" />
+                <h1>{name}</h1>
+                <p className='coin-symbol'>{symbol}</p>
+            </div>
+            <div className='coin-data'>
+                <p className="coin-price">${price}</p>
+                <p className="coin-volume">${volume.toLocaleString()}</p>
+                {priceChange < 0 ? (
+                    <p className='coin-present red'>{priceChange.toFixed(2)}%</p>
+                ) : (<p className='coin-present green'>{priceChange.toFixed(2)}%</p>)
+            }
+            <p className="coin-marketcap">
+                Mkt Cap : ${marketcap.toLocaleString()}
+            </p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Coin
